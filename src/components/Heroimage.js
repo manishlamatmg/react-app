f037aca18b7dc008c46e3966da47e6a50a53feb6
manishlamatmg <maneeshlam215@gmail.com>
2024-02-20 19:12:37 +0545

    first commit

---
+++ src/components/Heroimage.js
@@ -0,0 +1,26 @@
+import "./HeroimageStyles.css"
+
+import React from 'react'
+import introImg from "../assets/c.png"
+import { Link } from "react-router-dom"
+
+const Heroimage = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="into-img"
+            src={introImg} alt="introImg"/>
+        </div>
+      <div className="content">
+        <p>Elderly Care And Monitoring System</p>
+        <h1>ECMS</h1>
+        <div>
+            <Link to="/services" className="btn">Services</Link>
+            <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Heroimage
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,30 @@
 import { Link } from "react-router-dom"
 import "./NavbarStyles.css"
 
-import React from 'react'
+import React, { useState } from 'react'
+import {FaBars,FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
-    return (
-      <div className="header">
+  const [click,setClick]= useState(false);
+  const handleClick = () => setClick(!click);
+  
+  const [color,setColor]= useState(false);
+  const changeColor= () =>{
+    if(window.scrollY >=100){
+    setColor(true);
+  }else{
+    setColor(false);
+  }
+
+  };
+window.addEventListener("scroll", changeColor);
+
+  return (
+      <div className={color ? "header header-bg" : "header"}>
       <Link to="/">
           <h1>
               ECMS</h1></Link>
-              <ul className="nav-menu">
+              <ul className={click ?"nav-menu active" : "nav-menu"}>
                   <li>
                       <Link to="/">Home</Link> 
                       </li>
@@ -24,6 +39,14 @@ const Navbar = () => {
                       </li>
                       </ul>  
                       
+      
+      <div className="hambuger" onClick={handleClick}>
+        {click ? (<FaTimes size={20} style={{color: "#fff"}}/>) : (<FaBars size={20} style={{color: "#fff"}}/>)}
+        
+
+
+
+      </div>
       </div>
     )
   }
